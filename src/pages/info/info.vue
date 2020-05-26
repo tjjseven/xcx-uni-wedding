ca7cb8be63da2582ed0d70a57487e14c3accd37c
tjjseven <tang95279@163.com>
2020-05-26 17:58:07 +0800

    add take phone

--- src/pages/info/info.vue
+++ src/pages/info/info.vue
@@ -2,11 +2,18 @@
     <div class="t_content">
         <button open-type="getUserInfo" @getuserinfo="getUserInfo">获取信息</button>
         <button open-type="openSetting" @opensetting="openSetting">打开设置页</button>
+        <!--<button open-type="getPhoneNumber" @bindgetphonenumber="getPhoneNumber">获取手机号</button>-->
         <!--<web-view src="http://weixin.cyapp.net/cycloud"></web-view>-->
-        <img :src="headerImg" alt="">
+        <!--<img :src="headerImg" alt="">-->
         <open-data type="userNickName" lang="zh_CN"></open-data>
         <open-data type="userGender" lang="zh_CN"></open-data>
-        <button @click="getL">获取地址</button>
+        <button @click="autoLocation">获取地址</button>
+        <button @click="createCamera">拍照</button>
+        <button @click="imgSrc = ''">重拍</button>
+        <button @click="onload1">刷新</button>
+        <img :src="imgSrc" alt="" style="width: 100%; height: 300px;" v-if="imgSrc">
+        <camera device-position="back" flash="off" binderror="error" style="width: 100%; height: 300px;" v-if="!imgSrc"></camera>
+
         <navigator url="/package/detail/detail">canvas</navigator>
     </div>
 </template>
@@ -17,11 +24,16 @@
         name: "info",
         data () {
             return {
-                headerImg: ''
+                headerImg: '',
+                sysLocationEnabled: false,
+                wxLocationEnable: false,
+                cameraContext: null,
+                imgSrc: '',
             }
         },
         mounted () {
-            // uni.login({     // 登陆接口
+            // 登陆接口
+            // uni.login({
             //     provider:'weixin',
             //     scopes:'auth_base',
             //     timeout:20000,
@@ -29,73 +41,114 @@
             //     console.log(data)
             // })
 
-            wx.getSetting({ // 获取设置
-                success: (res) => {
-                    console.log(res)
-                    // if(res.authSetting["scope.userInfo"]){ // 已经授权
-                    //     wx.getUserInfo({
-                    //         success: (res) => {
-                    //             console.log(res)
-                    //             console.log(res.userInfo)
-                    //             this.headerImg = res.userInfo.avatarUrl
-                    //         }
-                    //     })
-                    // }else{ // 未授权
-                    //
-                    // }
 
+            // this.getSystemInfo()
+            // this.getSetting('location')
+            this.autoLocation()
 
-
-                    if(res.authSetting["scope.userLocation"]){
-                        this.getLocation()
-                    }else{
-                        wx.authorize({ // 调用授权 用户信息除外
-                            scope: 'scope.userLocation',
-                            success: () => {
-                                this.getLocation()
-                            },
-                            fail: () => {
-                                console.log(123)
-                                wx.showModal({
-                                    title: '授权失败提示',
-                                    content: '如需要授权您的位置，请打开设置',
-                                    confirmText: '需要',
-                                    cancelText: '不需要',
-                                    success (res) {
-                                        if (res.confirm) {
-                                            console.log('用户点击确定')
-                                            wx.openSetting({
-                                                success: (res) => {
-                                                    console.log(res.authSetting)
-                                                }
-                                            })
-                                        } else if (res.cancel) {
-                                            console.log('用户点击取消')
-                                        }
-                                    }
-                                })
-                            }
-                        })
-                    }
-                }
-            })
+            this.cameraContext = wx.createCameraContext()
         },
         methods: {
-            getL () {
-                wx.openSetting({
-                    success: (res) => {
-                        console.log(res.authSetting)
-                    }
-                })
-            },
+            /*按钮打开微信设置*/
             openSetting (e) {
                 console.log(e)
             },
+            /*按钮授权获取用户信息*/
             getUserInfo (e) {
                 console.log(e)
             },
+            /*按钮获取手机号 对非个人开发者，且完成了认证的小程序开放*/
+            getPhoneNumber (e) {
+                console.log(e)
+            },
+
+            /*获取手机相关功能权限信息*/
+            getSystemInfo () {
+                wx.getSystemInfo({
+                    success: (res) => {
+                        wx.showModal({
+                            title: '提示',
+                            content: JSON.stringify(res),
+                            showCancel: false,
+                            success (res) {
+                                if (res.confirm) {
+                                    console.log('用户点击确定')
+                                } else if (res.cancel) {
+                                    console.log('用户点击取消')
+                                }
+                            }
+                        })
+                    }
+                })
+            },
+            /*获取微信相关功能权限信息*/
+            getSetting (type) {
+                wx.getSetting({ // 获取设置
+                    success: (res) => {
+                        console.log(res)
+                        // if(res.authSetting["scope.userInfo"]){ // 已经授权
+                        //     wx.getUserInfo({
+                        //         success: (res) => {
+                        //             console.log(res)
+                        //             console.log(res.userInfo)
+                        //             this.headerImg = res.userInfo.avatarUrl
+                        //         }
+                        //     })
+                        // }else{ // 未授权
+                        //
+                        // }
+
+                        let systemInfo = wx.getSystemInfoSync() // 手机权限
+                        switch (type) {
+                            case 'location':
+                                if(!systemInfo.locationEnabled){
+                                    wx.showModal({
+                                        title: '授权提示',
+                                        content: '请开启手机系统定位功能',
+                                        showCancel: false,
+                                        success (res) {
+                                            if (res.confirm) {
+                                                console.log('用户点击确定')
+                                            } else if (res.cancel) {
+                                                console.log('用户点击取消')
+                                            }
+                                        }
+                                    })
+                                    return
+                                }
+                                if(!res.authSetting["scope.userLocation"]){
+                                    // this.wxLocationEnable = true
+                                    wx.showModal({
+                                        title: '授权提示',
+                                        content: '如需要授权您的位置，请打开设置',
+                                        confirmText: '需要',
+                                        cancelText: '不需要',
+                                        success (res) {
+                                            if (res.confirm) {
+                                                console.log('用户点击确定')
+                                                wx.openSetting({
+                                                    success: (res) => {
+                                                        console.log(res.authSetting)
+                                                    }
+                                                })
+                                            } else if (res.cancel) {
+                                                console.log('用户点击取消')
+                                            }
+                                        }
+                                    })
+                                    return
+                                }
+                                this.getLocation()
+                                break;
+                        }
+                    }
+                })
+            },
+
+            /*定位*/
             getLocation () {
                 wx.getLocation({
+                    isHighAccuracy: true,
                     success: (res) => {
                         console.log(res)
                         const latitude = res.latitude
@@ -105,8 +158,126 @@
                             longitude,
                             scale: 18
                         })
+                    },
+                    fail: (res) => {
+                        console.log(res)
                     }
                 })
+            },
+            /*自动定位*/
+            autoLocation () {
+                wx.authorize({ // 提前发起授权请求 用户信息除外
+                    scope: 'scope.userLocation',
+                    success: () => {
+                        let systemInfo = wx.getSystemInfoSync() // 手机权限
+                        if(!systemInfo.locationEnabled) {
+                            wx.showModal({
+                                title: '授权提示',
+                                content: '请开启手机系统定位功能',
+                                showCancel: false,
+                                success(res) {
+                                    if (res.confirm) {
+                                        console.log('用户点击确定')
+                                    } else if (res.cancel) {
+                                        console.log('用户点击取消')
+                                    }
+                                }
+                            })
+                        }else{
+                            this.getLocation()
+                        }
+                    },
+                    fail: () => {
+                        console.log(123)
+                        wx.showModal({
+                            title: '授权失败提示',
+                            content: '如需要授权您的位置，请打开设置',
+                            confirmText: '需要',
+                            cancelText: '不需要',
+                            success: (res) => {
+                                if (res.confirm) {
+                                    console.log('用户点击确定')
+                                    wx.openSetting({
+                                        success: (res) => {
+                                            console.log(res.authSetting)
+                                            wx.showToast({
+                                                title: '授权成功',
+                                                icon: 'none',
+                                            })
+                                            if(res.authSetting['scope.userLocation']){
+                                                // this.autoLocation()
+                                                console.log(this)
+                                                console.log(wx)
+
+
+                                            }
+                                        }
+                                    })
+                                } else if (res.cancel) {
+                                    console.log('用户点击取消')
+                                }
+                            }
+                        })
+                    }
+                })
+            },
+            createCamera () {
+                wx.authorize({ // 提前发起授权请求 用户信息除外
+                    scope: 'scope.camera',
+                    success: () => {
+                        this.cameraContext.takePhoto({
+                            success: (res) => {
+                                console.log(res)
+                                wx.showToast({
+                                    title: this.imgSrc,
+                                    icon: 'none',
+                                })
+                                this.imgSrc = res.tempImagePath
+                            },
+                            fail: (res) => {
+                                console.log(res)
+                            },
+                        })
+                    },
+                    fail: () => {
+                        wx.showModal({
+                            title: '授权失败提示',
+                            content: '如需要拍照，请打开设置',
+                            confirmText: '需要',
+                            cancelText: '不需要',
+                            success: (res) => {
+                                if (res.confirm) {
+                                    console.log('用户点击确定')
+                                    wx.openSetting({
+                                        success: (res) => {
+                                            console.log(res.authSetting)
+                                            if(res.authSetting['scope.camera']){
+
+                                                // wx.showToast({
+                                                //     title: this.imgSrc,
+                                                //     icon: 'none',
+                                                // })
+                                            }
+                                        }
+                                    })
+                                } else if (res.cancel) {
+                                    console.log('用户点击取消')
+                                }
+                            }
+                        })
+                    }
+                })
+
+
+            },
+            onload1 () {
+                const pages = getCurrentPages()
+                console.log(pages)
+                const perpage = pages[pages.length - 1]
+                console.log(perpage)
+                perpage.onLoad()
+                // let url = '/pages/com/com'
+                // uni.reLaunch({ url })
             }
         }
 
